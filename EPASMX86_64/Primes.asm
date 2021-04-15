ffb797beaf4cd56ac2bd8579ee5aa0e65ee3b4f0
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2021-04-15 13:12:20 +1000

    Cut down on memory accesses in asm version

--- EPASMX86_64/Primes.asm
+++ EPASMX86_64/Primes.asm
@@ -3,28 +3,30 @@ extern malloc: PROC
 .code
 
 generate_primes PROC
+	; Rather than continually pushing and poping n and pSize to get around malloc overridding them since they are volatile
+	; some non volatile registers can be backed up and then used to house n and pSize
 	; Backup n and pSize
-	push rdx
-	push rcx
+	push r12
+	push r13
+	mov r12, rcx
+	mov r13, rdx
 
 	; Allocate boolean array
 	sub rsp, 20h
 	call malloc
 	add rsp, 20h
 
-	pop rcx ; n
-
 	mov r8, 2
 booleanFillLoop:
 	mov byte ptr [rax + r8], 1
 	inc r8
-	cmp r8, rcx
+	cmp r8, r12
 	jne booleanFillLoop
 
-	; Performs r11 = ceil(sqrt(rcx)) for max factor
+	; Performs r11 = ceil(sqrt(r12)) for max factor, sqrt is done here and ceil is done implicitly using comparisons
 	; Assumes sse2 support, todo check first and use fpu otherwise? 
 	; sse2 support is in x86_64 baseline though, it might be fine to assume support
-	cvtsi2sd xmm0, rcx
+	cvtsi2sd xmm0, r12
 	sqrtsd xmm0, xmm0
 	cvttsd2si r11, xmm0
 
@@ -42,21 +44,19 @@ primeCheckLoop:
 	mov r10, r8
 	shl r10, 1
 primeCheckLoopInner:
-	cmp r10, rcx
+	cmp r10, r12
 	jnl primeCheckLoopIterate
 	mov byte ptr [rax + r10], 0
 	add r10, r8
 	jmp primeCheckLoopInner
 primeCheckLoopIterate:
 	inc r8
-	cmp r8, rcx
+	cmp r8, r12
 	jl primeCheckLoop
 	
-	pop rdx ; pSize
-	push rax ; bool array
-	push rcx ; n
+	mov dword ptr [r13], r9d
 
-	mov dword ptr [rdx], r9d
+	mov r13, rax ; bool array
 
 	; Allocate integer array
 	shl r9, 2
@@ -65,20 +65,20 @@ primeCheckLoopIterate:
 	call malloc
 	add rsp, 20h
 
-	pop rcx ; n
-	pop rdx ; bool array
-
 	mov r8, 2; loop counter/bool array indexer
-		mov r9, 0; int array indexer
+	mov r9, 0; int array indexer
 primeFillLoop:
-	cmp byte ptr [rdx + r8], 1
+	cmp byte ptr [r13 + r8], 1
 	jne primeFillLoopIterate
 	mov dword ptr [rax + r9], r8d
 	add r9, 4
 primeFillLoopIterate:
 	inc r8
-	cmp r8, rcx
+	cmp r8, r12
 	jng primeFillLoop
+
+	pop r13
+	pop r12
 
 	ret
 generate_primes ENDP
